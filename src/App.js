6a6dc23dc7612cd58543a05e623c818c423276a1
venky4c <venky4c@gmail.com>
2020-08-25 18:46:45 +0530

    iamges from unsplash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 export default function App() {
+  const [images, setImages] = useState();
+  // fetch(
+  //   "https://api.unsplash.com/photos/?client_id=UMd3gSEjAXU2rrhv2dgZGWObqbWTKX4yv6vXQmr7go8"
+  // )
+  useEffect(() => {
+    console.log("access key is", accessKey);
+    fetch(`https://api.unsplash.com/photos/?client_id=${accessKey}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data);
+      });
+  }, []);
+
+  // return an error if there is no access key
+  // if (!accessKey) {
+  //   return (
+  //     <a href="https://unsplash.com/developers" className="error">
+  //       Required: Get Your Unsplash API Key First
+  //     </a>
+  //   );
+  // }
   return (
     <div className="app">
       <h1>Unsplash Image Gallery!</h1>
@@ -14,7 +35,7 @@ export default function App() {
       <div className="image-grid">
         {[...Array(100)].map((_, index) => (
           <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+            <img src="{image.alt_description.regular}" alt="Sample" />
           </div>
         ))}
       </div>
